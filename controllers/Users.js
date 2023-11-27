7d3c095a311d1a201f4d4ba25d5c3d2b426c6c11
IifAlifah <you@example.com>
2023-11-28 01:26:14 +0800

    Register

---
+++ controllers/Users.js
@@ -0,0 +1,42 @@
+import Users from "../models/UserModel.js";
+import bcrypt from "bcrypt";
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await Users.findAll();
+        res.json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "Terjadi kesalahan server" });
+    }
+};
+
+export const Register = async (req, res) => {
+    const { name, username, email, password, confPassword } = req.body;
+
+    if (password !== confPassword) {
+        return res.status(400).json({ msg: "Password dan Confirm Password tidak cocok" });
+    }
+
+    try {
+        const existingUser = await Users.findOne({ where: { email: email } });
+
+        if (existingUser) {
+            return res.status(400).json({ msg: "Email sudah terdaftar" });
+        }
+
+        const salt = await bcrypt.genSalt();
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        await Users.create({
+            name: name,
+            username: username,
+            email: email,
+            password: hashPassword
+        });
+
+        res.json({ msg: "Register Berhasil" });
+    } catch (error) {
+        console.log(error);
+    }
+};
---
+++ models/UserModel.js
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
+
+const { DataTypes } = Sequelize;
+
+const Users = db.define('users', {
+    name: {
+        type: DataTypes.STRING
+    },
+    username: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    refresh_token: {
+        type: DataTypes.TEXT
+    },
+    balance: {
+        type: DataTypes.INTEGER, 
+        defaultValue: 0
+    }
+}, {
+    freezeTableName: true
+});
+
+export default Users;

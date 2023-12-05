c43c3dcc697e1f996214b2030542505981af4a8b
Akhmad Sugiannoor <AkhmadSugiannoor9@gmail.com>
2023-12-05 12:37:05 +0800

    Menambah API Edit Delete User dan Menghapus Header GetUser

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
+import { getUsers, Register, Login, Logout, editUser, getUserById, deleteUser } from "../controllers/Users.js";
 import { verify_Token } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 
 const router = express.Router();
 
-router.get('/users', verify_Token, getUsers);
+router.get('/users', getUsers);
 router.post('/users', Register); 
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
+router.put('/users/:id', editUser)
+router.get('/users/:id', getUserById);
+router.delete('/users/:id', deleteUser);
 
 export default router;

d4d33a7ec9f20fe041c254f4b8a6cb9379068479
DelphiaAryana <delphiaaryana25@gmail.com>
2023-12-06 19:33:09 +0800

    Menambahkan kode foreign key, getTableTransaksi, dan createTransaksi

--- controllers/TransaksiController.js
+++ controllers/TransaksiController.js
@@ -1,16 +1,60 @@
 import Transaksi from '../models/TransaksiModel.js';
+import Users from '../models/UserModel.js';
+import Item from '../models/ItemModel.js';
 
 export const getTransaksi = async (req, res) => {
-    try {
-      const transaksi = await Transaksi.findAll({
-        attributes: ['id', 'name', 'item','price', 'quantity', 'alamat', 'date', 'total'],
-      });
-      res.json(transaksi);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ msg: 'Terjadi kesalahan server' });
-    }
-  };
+  try {
+    const transaksi = await Transaksi.findAll({
+      attributes: ['id', 'id_user', 'id_item', 'noHp', 'quantity', 'address', 'total'],
+      include: [
+        { model: Users, attributes: ['id'] },
+        { model: Item, attributes: ['id'] } 
+      ],
+    });
+    res.json(transaksi);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: 'Terjadi kesalahan server' });
+  }
+};
+
+export const getTableTransaksi = async (req, res) => {
+  try {
+    const transaksi = await Transaksi.findAll({
+      attributes: ['id', 'noHp', 'quantity', 'address', 'total'],
+      include: [
+        {
+          model: Users,
+          attributes: ['id', 'name'], 
+        },
+        {
+          model: Item,
+          attributes: ['id', 'name', 'price'], 
+        },
+      ],
+    });
+
+    const formattedTransaksi = transaksi.map((trans) => ({
+      id: trans.id,
+      id_user: trans.User.id,
+      id_item: trans.Item.id,
+      noHp: trans.noHp,
+      quantity: trans.quantity,
+      address: trans.address,
+      date: trans.createdAt,
+      total: trans.total,
+      user_name: trans.User.name, 
+      item_name: trans.Item.name, 
+      item_price: trans.Item.price,
+    }));
+
+    res.json(formattedTransaksi);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: 'Terjadi kesalahan server' });
+  }
+};
+
   
   export const getTransaksiById = async (req, res) => {
     try {
@@ -28,4 +72,30 @@ export const getTransaksi = async (req, res) => {
       res.status(500).json({ msg: 'Terjadi kesalahan server' });
     }
   };
+
+  export const createTransaksi = async (req, res) => {
+    try {
+      const { id_user, id_item, noHp, quantity, address} = req.body;
+  
+      const item = await Item.findByPk(id_item);
+      const harga = item ? item.price : 0;
+
+      const total = harga * quantity;
+
+      const newTransaksi = await Transaksi.create({
+        id_user,
+        id_item,
+        noHp,
+        quantity,
+        address,
+        total,
+      });
+  
+      res.status(201).json(newTransaksi);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: 'Terjadi kesalahan server' });
+    }
+  };
+  
   

53f07dc603c931d9a4837faa3a27104302285e9d
DelphiaAryana <delphiaaryana25@gmail.com>
2023-12-07 20:17:40 +0800

    Session Login

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import session from "express-session";
+import SequelizeStore  from "connect-session-sequelize";
 import db from './config/database.js';
 import router from './routes/index.js';
+import AuthRoute from './routes/AuthRoute.js';
 import FileUpload from "express-fileupload";
 import ItemRoute from "./routes/ItemRoute.js";
 import TransaksiRoute from "./routes/TransaksiRoute.js";
 
 dotenv.config();
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+  db: db
+})
+
 try {
   await db.authenticate();
   console.log('Database Connected');
@@ -18,13 +28,26 @@ try {
   console.error(error);
 }
 
+app.use(session({
+  secret: process.env.SESS_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: store,
+  cookie: {
+    secure: 'auto'
+  }
+}))
+
 app.use(cors({ credentials: true, origin: 'http://127.0.0.1:5173' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(FileUpload());
 app.use(express.static("public"));
 app.use(router);
+app.use(AuthRoute);
 app.use(ItemRoute);
 app.use(TransaksiRoute);
 
-app.listen(4000, () => console.log('Server running at port 4000'));
+store.sync();
+
+app.listen(process.env.APP_URL, () => console.log('Server running at port 4000'));

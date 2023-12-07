1082070aeca0d5daae6c9e3bf69ac975c3a5b22f
DelphiaAryana <delphiaaryana25@gmail.com>
2023-12-07 20:18:52 +0800

    Role Admin

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,17 +4,51 @@ import db from '../config/database.js';
 const { DataTypes } = Sequelize;
 
 const Users = db.define('users', {
+  uuid: {
+    type: DataTypes.STRING,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
   name: {
     type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      len: [3, 100]
+    }
   },
   username: {
     type: DataTypes.STRING,
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   email: {
     type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
   },
   password: {
     type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   refresh_token: {
     type: DataTypes.TEXT,

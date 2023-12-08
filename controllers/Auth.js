29be6293a6d1a3a4469c609919d2f3ed6e61e5e4
DelphiaAryana <delphiaaryana25@gmail.com>
2023-12-08 10:30:21 +0800

    Menambahkan attribute id

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -11,11 +11,12 @@ export const Login = async(req, res) => {
     const match = await argon2.verify(user.password, req.body.password);
     if(!match) return res.status(400).json({msg: "Password salah"});
     req.session.userId = user.uuid;
+    const id = user.id;
     const uuid = user.uuid;
     const name = user.name;
     const email = user.email;
     const role = user.role;
-    res.status(200).json({uuid, name, email, role});
+    res.status(200).json({id, uuid, name, email, role});
 }
 
 export const Me = async (req, res) =>{
@@ -24,7 +25,7 @@ export const Me = async (req, res) =>{
 
     }
     const user = await Users.findOne({
-        attributes:['uuid', 'name', 'email', 'role'],
+        attributes:['id', 'uuid', 'name', 'email', 'role'],
         where: {
             uuid: req.session.userId
         }

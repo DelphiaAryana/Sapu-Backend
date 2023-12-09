ff314d6a6ea5790ff7930e2638f9aacf3d6b4e7d
IifAlifah <you@example.com>
2023-12-09 17:40:36 +0800

    memperbaiki

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 const db = new Sequelize('sapu_db', 'root', '', {
-  host: process.env.DB_HOST,
+  host: 'localhost',
   dialect: 'mysql',
 });
 export default db;

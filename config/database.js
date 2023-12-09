856103fb6add84cdb9468f24fdd79a94d0d773d0
DelphiaAryana <delphiaaryana25@gmail.com>
2023-12-09 22:07:13 +0800

    Vercel

--- config/database.js
+++ config/database.js
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 
-const db = new Sequelize('bzzt4lortqptla9xotfm', 'umdjjxf57nugao03', '9BhhUjjHMXHNtietC1yO', {
+const db = new Sequelize({
+  database: 'bzzt4lortqptla9xotfm',
+  username: 'umdjjxf57nugao03',
+  password: '9BhhUjjHMXHNtietC1yO',
   host: 'bzzt4lortqptla9xotfm-mysql.services.clever-cloud.com',
   dialect: 'mysql',
+  dialectModule: mysql2,
 });
 export default db;

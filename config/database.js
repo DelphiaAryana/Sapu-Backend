31ffee113ab00f1430ccff44177761b9ab6ef110
IifAlifah <you@example.com>
2023-12-09 17:34:22 +0800

    vercel

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const db = new Sequelize('sapu_db', 'root', '', {
   host: process.env.DB_HOST,
   dialect: 'mysql',
 });
